da467767accf036d4b7c8cf65e36c88cca6e2d20
Clémence Touré <92016967+Clemencetr@users.noreply.github.com>
2022-09-21 16:52:00 +0200

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -308,6 +308,25 @@ $(function(){
         $("#myModal11").css("display", "none");
     });
 
+
+     //quand on clique sur une image
+     $("#journeepatrimoine img").on('click', function(){
+        //affichage de la modal en modifiant le css de la modal
+        $("#myModal12").css("display", "flex");
+
+        //ajout de la source de l'image dans le contenu de la modal en fonction de la source de l'image cliquée
+        $(".web").attr("src", $(this).attr("src"));
+
+        //affiche bouton fermeture
+        $(".close").css("display", "block");
+    });
+
+    
+    //si on clique sur le bouton, on ferme la modal en modifiant le css
+    $(".close").on("click", function(){
+        $("#myModal12").css("display", "none");
+    });
+
     
     
 
